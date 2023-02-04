5f3fbc17e4a44e3a94a29557edc58eae48754cac
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-02-04 23:03:56 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (58.67%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start-(start-end)/2;
+            if(arr[mid]>target){
+                end=mid-1;
+            }else if(arr[mid]<target){
+                start=mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
