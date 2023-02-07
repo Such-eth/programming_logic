84d1ca32001874858b2e3ff43a00c4f517d885bd
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-02-07 22:39:25 +0530

    Time: 1 ms (99.22%), Space: 40.2 MB (71.39%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,20 @@
+import java.util.Stack;  
+class Solution {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char a=s.charAt(i);
+            if(a=='(' || a=='{' || a=='['){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.isEmpty()){return false;}
+                char b=stack.pop();
+                if(a=='}'&&b!='{' || a==']'&&b!='[' || a==')'&&b!='('){
+                    return false;
+                }
+            }
+        }
+        if(stack.empty()){return true;}
+        return false;
+    }
+}
