db062234db5b34d2f05231ac83b89c9b66075f68
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-02-10 11:53:27 +0530

    Time: 14 ms (99.92%), Space: 41.6 MB (95.79%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        return ispow(n,0);
-    }
-    public boolean ispow(int n,int i){
-        double a=Math.pow(3,i);
-        if(a==n){return true;}
-        if(a>n){return false;}
-        return ispow(n,++i);
+        return n > 0 && 1162261467 % n == 0;
     }
 }
