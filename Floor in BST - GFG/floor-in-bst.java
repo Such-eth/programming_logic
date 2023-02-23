cd236e7b15954593146b88597f4642072f85f363
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-02-23 20:06:30 +0530

    Added solution - LeetHub

---
+++ Floor in BST - GFG/floor-in-bst.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node right;
+    Node left;
+
+    Node(int val) {
+        data = val;
+        right = null;
+        left = null;
+    }
+}
+
+class GFG {
+
+    public static Node insert(Node tree, int val) {
+        Node temp = null;
+        if (tree == null) {
+            return new Node(val);
+        }
+
+        if (val < tree.data) {
+            tree.left = insert(tree.left, val);
+        } else if (val > tree.data) {
+            tree.right = insert(tree.right, val);
+        }
+
+        return tree;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            Node root = null;
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int k = sc.nextInt();
+                root = insert(root, k);
+            }
+
+            int s = sc.nextInt();
+
+            Solution obj = new Solution();
+            int ans = obj.floor(root, s);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int ans=0;
+    public static int floor(Node root, int x) {
+        // Code here
+        ans=-1;
+        helper(root,x);
+        return ans;
+    }
+    public static void helper(Node root, int x){
+        if(root!=null){
+            helper(root.left,x);
+            if(root.data<=x){
+                ans=root.data;
+            }
+            helper(root.right,x);
+        }
+    }
+}
