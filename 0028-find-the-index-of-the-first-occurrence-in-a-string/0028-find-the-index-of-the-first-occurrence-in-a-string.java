4918deab4564f46e6f9b2609f49c29c89c79fdd0
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-03-24 10:27:07 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (29.66%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m=haystack.length();
+        int n=needle.length();
+        if(m<n){return -1;}
+        for(int i=0;i<m-n+1;i++){
+            String s=haystack.substring(i,i+n);
+            if(s.equals(needle)){return i;}
+        }
+        return -1;
+    }
+}
