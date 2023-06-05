edfee10855cff178ea8ef306f2b55206c3832887
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-06-05 23:23:35 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (60.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,29 @@
 class Solution {
-    static int count=0;
+    static int[] arr;
     public static int climbStairs(int n) {
-        if(n==1) return 1;
+//         if(n==1) return 1;
         
-        if(n==2) return 2;
+//         if(n==2) return 2;
 
-        int[] a =  new int[n];
-        a[0]=1;
-        a[1]=2;
+//         int[] a =  new int[n];
+//         a[0]=1;
+//         a[1]=2;
 
-        for(int i=2;i<n;i++){
-            a[i]=a[i-1]+a[i-2];
-        }
-        return a[n-1];
+//         for(int i=2;i<n;i++){
+//             a[i]=a[i-1]+a[i-2];
+//         }
+//         return a[n-1];
+        arr=new int[n];
+        return helper(n);
+    }
+    public static int helper(int n){
+        if(n==0)
+            return 1;
+        if(n<0)
+            return 0;
+        if(arr[n-1]>0)
+            return arr[n-1];
+        arr[n-1]=helper(n-1)+helper(n-2);
+        return helper(n-1)+helper(n-2);
     }
 }
