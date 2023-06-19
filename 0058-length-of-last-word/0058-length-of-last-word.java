2131d7c5b83a41212b7431a0038d0ebe7a52d3c1
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-06-20 00:00:27 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (79.88%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int ans = 0;
-        String[] arr = s.split(" ");
-		
-		return arr[arr.length - 1].length();
+		// String[] arr = s.split(" ");		
+		// return arr[arr.length - 1].length();
+        int i=s.length()-1;
+        int count=0;
+        while(i>=0 && s.charAt(i)==' '){
+            i--;
+        }
+        while(i>=0 && s.charAt(i)!=' '){
+            count++;
+            i--;
+        }
+        return count;
     }
 }
