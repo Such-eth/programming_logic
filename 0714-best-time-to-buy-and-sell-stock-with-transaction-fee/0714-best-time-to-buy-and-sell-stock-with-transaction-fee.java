50f7dbc5b59152f765b9e832320770497bd1d154
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-07-07 00:01:51 +0530

    Time: 4 ms (95.61%), Space: 54.2 MB (93.79%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,19 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int[][] dp = new int[prices.length+1][2];
-        dp[prices.length][0]=0;
-        dp[prices.length][1]=0;
+        int[] dp = new int[prices.length+1];
+        dp[prices.length]=0;
+        dp[prices.length]=0;
         int profit=0;
+        int[] mid = new int[2];
         for(int ind=prices.length-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 if(buy==1){
-                    profit=Math.max(dp[ind+1][0]-prices[ind]-fee,dp[ind+1][1]);
+                    profit=Math.max(mid[0]-prices[ind]-fee,mid[1]);
                 }else{
-                     profit=Math.max(dp[ind+1][1]+prices[ind],dp[ind+1][0]);
+                     profit=Math.max(mid[1]+prices[ind],mid[0]);
                 }
-                dp[ind][buy] = profit;
+                mid[buy] = profit;
             }
         }
-        return dp[0][1];
+        return mid[1];
     }
 }
