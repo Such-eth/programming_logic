81a9c527da915c4cc0b0a38cf3a436df9ec7243d
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-07-06 23:41:31 +0530

    Time: 1 ms (82.78%), Space: 40.8 MB (51.61%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,25 @@
+class Solution {
+    int[][] dp;
+    public int maxProfit(int[] prices) {
+        dp = new int[prices.length][2];
+        for(int[] i:dp){
+            Arrays.fill(i,-1);
+        }
+        return maxi(prices,0,1);
+    }
+    public int maxi(int[] arr,int ind,int buy){
+        if(ind>=arr.length){
+            return 0;
+        }
+        if(dp[ind][buy]>-1){
+            return dp[ind][buy];
+        }
+        int profit=0;
+        if(buy==1){
+            profit=Math.max(maxi(arr,ind+1,0)-arr[ind],maxi(arr,ind+1,1));
+        }else{
+            profit=Math.max(maxi(arr,ind+2,1)+arr[ind],maxi(arr,ind+1,0));
+        }
+        return dp[ind][buy]=profit;
+    }
+}
