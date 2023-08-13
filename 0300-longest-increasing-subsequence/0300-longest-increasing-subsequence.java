2c8eac9939f6b7a36909e015c315a9f2b61cd72c
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-08-13 11:26:19 +0530

    Time: 355 ms (15.37%), Space: 100.1 MB (5.56%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,35 @@
 class Solution {
-    public static int[][] dp;
-    public static int lengthOfLIS(int[] nums) {
+    public int[][] dp;
+    public int lengthOfLIS(int[] nums) {
         dp = new int[nums.length][nums.length];
         for(int[] i:dp)
             Arrays.fill(i,-1);
         return find_size(nums,0,-1);
     }
-    private static int find_size(int[] arr, int i, int j) {
-        if(i==arr.length){
-            return 0;
-        }
-        if(dp[i][j+1]>-1){
-            return dp[i][j+1];
-        }
-        int exc = find_size(arr,i+1,j);
-        int inc=0;
-        if(j==-1 || arr[i]>arr[j])
-            inc = 1+find_size(arr,i+1,i);
-        dp[i][j+1]=Math.max(exc,inc);
-        return dp[i][j+1];
+    
+    public int find_size(int[] arr,int i,int j){
+        if(i==arr.length)
+            return 0;        
+        if(j==-1)
+            return Math.max(1+find_size(arr,i+1,i),find_size(arr,i+1,j));
+        if(dp[i][j]>-1)
+            return dp[i][j];
+        if(arr[j]<arr[i])
+            return dp[i][j]=Math.max(1+find_size(arr,i+1,i),find_size(arr,i+1,j));
+        return dp[i][j]=find_size(arr,i+1,j);
     }
+    
+//     public static int binaary_search(int arr,int num){
+//         int start = 0;
+//         int end = num.length-1;
+//         int mid = start-(start-end)/2;
+//         int ans=0;
+//         if(arr[mid]==num)
+//             return mid;
+//         if(arr[mid]<num){
+            
+//         }else{
+            
+//         }
+//     }
 }
