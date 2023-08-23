e3aba868cc646ec45290e5e65370e3caa03d7e6e
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-08-23 11:11:33 +0530

    Time: 1 ms (82.68%), Space: 40.3 MB (76.59%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,12 +1,29 @@
 class Solution {
-     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++)
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) return "";
-            }        
-        return prefix;
+    //  public String longestCommonPrefix(String[] strs) {
+    //     if (strs.length == 0) return "";
+    //     String prefix = strs[0];
+    //     for (int i = 1; i < strs.length; i++)
+    //         while (strs[i].indexOf(prefix) != 0) {
+    //             prefix = prefix.substring(0, prefix.length() - 1);
+    //             if (prefix.isEmpty()) return "";
+    //         }        
+    //     return prefix;
+    // }
+    
+    public String longestCommonPrefix(String[] strs){
+        String ans="";
+        for(int i=0;i<strs[0].length();i++){
+            char check = strs[0].charAt(i);
+            int j;
+            for(j=1;j<strs.length && i<strs[j].length();j++){
+                if(strs[j].charAt(i)!=check)
+                    break;
+            }
+            if(j==strs.length)
+                ans+=String.valueOf(check);
+            else
+                return ans;
+        }
+        return ans;
     }
 }
