05274b1cda96c1be1f88e6c9fe8fea329036b79f
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-09-09 12:04:58 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (58.11%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -16,11 +16,18 @@
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root==null){return 0;}
-        return helper(root);
+        return maxi(root);
     }
     public int helper(TreeNode node){
         if(node==null)
             return 0;
         return 1+Math.max(helper(node.left),helper(node.right));
+    }
+    public int maxi(TreeNode node){
+        if(node==null)
+            return 0;
+        int left = maxi(node.left);
+        int right = maxi(node.right);
+        return Math.max(left,right)+1;
     }
 }
