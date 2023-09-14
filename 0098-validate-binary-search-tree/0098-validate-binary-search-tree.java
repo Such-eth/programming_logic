ad1928229193ab6adca339380701036b2a902f61
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-09-14 16:55:16 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (53.02%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -16,11 +16,18 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         if(root.left==null && root.right==null){return true;}
-        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        return validate(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
     public boolean helper(TreeNode node,long min,long max){
         if(node==null){return true;}
         if(node.val<=min || node.val>=max){return false;}
         return helper(node.left,min,node.val) && helper(node.right,node.val,max);
+    }
+    public boolean validate(TreeNode node,long left,long right){
+        if(node==null)
+            return true;
+        if(node.val<=left || node.val>=right)
+            return false;
+        return validate(node.left,left,node.val)&&validate(node.right,node.val,right);
     }
 }
