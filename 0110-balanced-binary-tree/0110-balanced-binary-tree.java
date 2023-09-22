6b6060f372a6407ad5444bcbb6829a4fb8896b01
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-09-22 22:08:19 +0530

    Time: 1 ms (41.12%), Space: 43 MB (73.72%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,22 +14,40 @@
  * }
  */
 class Solution {
+    // public boolean isBalanced(TreeNode root) {
+        // if(root==null)
+        //     return true;
+        // if(check(root)==-1)
+        //     return false;
+        // return true;
+        
+    // }
+    // public int check(TreeNode root){
+    //     if(root==null)
+    //         return 0;
+    //     int left=check(root.left);
+    //     int right=check(root.right);
+    //     if(left==-1 || right==-1)
+    //         return -1;
+    //     if(Math.abs(left-right)>1)
+    //         return -1;
+    //     return 1+Math.max(left,right);
+    // }
     public boolean isBalanced(TreeNode root) {
         if(root==null)
             return true;
-        if(check(root)==-1)
+        int left = height(root.left);
+        int right = height(root.right);
+        int diff = Math.abs(left-right);
+        if(diff!=0 && diff!=1)
             return false;
-        return true;
+        return isBalanced(root.left)&&isBalanced(root.right);
     }
-    public int check(TreeNode root){
-        if(root==null)
+    public int height(TreeNode node){
+        if(node==null)
             return 0;
-        int left=check(root.left);
-        int right=check(root.right);
-        if(left==-1 || right==-1)
-            return -1;
-        if(Math.abs(left-right)>1)
-            return -1;
-        return 1+Math.max(left,right);
+        int left = height(node.left);
+        int right = height(node.right);
+        return Math.max(left,right)+1;
     }
 }
