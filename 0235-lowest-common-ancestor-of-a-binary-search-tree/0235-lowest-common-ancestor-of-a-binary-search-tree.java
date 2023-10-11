de73c7ebcce00d2bc18d6e12a02e38a23009e76f
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-10-11 14:44:34 +0530

    Time: 5 ms (100.00%), Space: 44.6 MB (6.15%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,14 +9,29 @@
  */
 
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null){return null;}
-        if(p.val<root.val && q.val<root.val){
-            return  lowestCommonAncestor(root.left,p,q);
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     if(root==null){return null;}
+    //     if(p.val<root.val && q.val<root.val){
+    //         return  lowestCommonAncestor(root.left,p,q);
+    //     }
+    //     if(p.val>root.val && q.val>root.val){
+    //         return  lowestCommonAncestor(root.right,p,q);
+    //     }
+    //     return root;
+    // }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        return lca(root,p,q);
+    }
+    public TreeNode lca(TreeNode root, TreeNode p, TreeNode q){
+        if(root==null)
+            return null;
+        if(root==p || root==q)
+            return root;
+        if(root.val>p.val && root.val>q.val){
+            return lca(root.left,p,q);
         }
-        if(p.val>root.val && q.val>root.val){
-            return  lowestCommonAncestor(root.right,p,q);
-        }
+        if(root.val<p.val && root.val<q.val)
+            return lca(root.right,p,q);
         return root;
     }
 }
