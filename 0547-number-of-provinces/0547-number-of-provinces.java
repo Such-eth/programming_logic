b40138a42bea7b2a25211446adb55d500e48c4ea
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-10-27 06:56:41 +0530

    Time: 13 ms (5.14%), Space: 46 MB (41.13%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,41 +1,59 @@
 class Solution {
     public int[] visited;
+    public int x;
     public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i=0;i<=isConnected.length;i++){
-            graph.add(new ArrayList<>());
-        }
-        for(int i=0;i<isConnected[0].length;i++){
-            for (int j=0;j<isConnected.length;j++){
-                if(isConnected[j][i]==1 ){
-                    graph.get(j+1).add(i+1);
-                }
-            }
-        }
-        visited = new int[isConnected.length+1];
+        // List<List<Integer>> graph = new ArrayList<>();
+        // for(int i=0;i<=isConnected.length;i++){
+        //     graph.add(new ArrayList<>());
+        // }
+        // for(int i=0;i<isConnected[0].length;i++){
+        //     for (int j=0;j<isConnected.length;j++){
+        //         if(isConnected[j][i]==1 ){
+        //             graph.get(j+1).add(i+1);
+        //         }
+        //     }
+        // }
+        visited = new int[isConnected.length];
         int ans=0;
-        for(int i=1;i<visited.length;i++){
+        x=0;
+        for(int i=0;i<visited.length;i++){
             if(visited[i]==0){
                 ans++;
-                dfs(graph,i);
+                dfs(isConnected,i);
             }
         }
         return ans;
     }
 
-    private int[] dfs(List<List<Integer>> graph,int head) {
+    // private int[] dfs(List<List<Integer>> graph,int head) {
+    //     Queue<Integer> q = new LinkedList<>();
+    //     q.add(head);
+    //     visited[head]=1;
+    //     while(!q.isEmpty()){
+    //         Integer curr = q.poll();
+    //         for(int num : graph.get(curr)){
+    //             if(visited[num]==0){
+    //                 visited[num]=1;
+    //                 q.add(num);
+    //             }
+    //         }
+    //     }
+    //     return visited;
+    // }
+    
+    private void dfs(int[][] graph, int node){
         Queue<Integer> q = new LinkedList<>();
-        q.add(head);
-        visited[head]=1;
+        q.offer(node);
+        visited[node]=1;
         while(!q.isEmpty()){
             Integer curr = q.poll();
-            for(int num : graph.get(curr)){
-                if(visited[num]==0){
-                    visited[num]=1;
-                    q.add(num);
+            for(int i=0;i<graph.length;i++){
+                if(graph[curr][i]==1 && visited[i]==0){
+                    visited[curr]=1;
+                    q.offer(i);
                 }
             }
         }
-        return visited;
+        // System.out.println(x++ +" ");
     }
 }
