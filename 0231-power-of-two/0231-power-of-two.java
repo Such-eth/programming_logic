517ed671af56d818c14aef08323b031f328fe3bf
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-11-29 08:10:09 +0530

    Time: 1 ms (86.01%), Space: 39.6 MB (51.67%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return ispow(n,0);
+        if(n>0 && (n&n-1)==0)
+            return true;
+        return false;
+        // return ispow(n,0);
     }
     public boolean ispow(int n,int i){
         double a=Math.pow(2,i);
