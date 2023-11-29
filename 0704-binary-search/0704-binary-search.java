429b12d5e8fe7d00278f1f9cb0532dd822871f7e
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-11-29 08:00:28 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (50.18%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -2,16 +2,30 @@ class Solution {
     public int search(int[] arr, int target) {
         int start=0;
         int end=arr.length-1;
-        while(start<=end){
-            int mid=start-(start-end)/2;
-            if(arr[mid]>target){
-                end=mid-1;
-            }else if(arr[mid]<target){
-                start=mid+1;
-            }else{
-                return mid;
-            }
+        // while(start<=end){
+        //     int mid=start-(start-end)/2;
+        //     if(arr[mid]>target){
+        //         end=mid-1;
+        //     }else if(arr[mid]<target){
+        //         start=mid+1;
+        //     }else{
+        //         return mid;
+        //     }
+        // }
+        return bin_search(arr,start,end,target);
+    }
+    public int bin_search(int[] arr,int start,int end, int target){
+        if(start>end)
+            return -1;
+        int mid = start-(start-end)/2;
+        if(arr[mid]>target){
+            end=mid-1;
+            return bin_search(arr,start,end,target);
+        }else if(arr[mid]<target){
+            start=mid+1;
+            return bin_search(arr,start,end,target);
+        }else{
+            return mid;
         }
-        return -1;
     }
 }
