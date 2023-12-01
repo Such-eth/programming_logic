876735e62ef23b32284bba4e29f5fe6b8f6867dc
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-12-01 07:11:34 +0530

    Time: 2 ms (41.64%), Space: 43.9 MB (35.18%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,23 @@
 class Solution {
+    List<List<Integer>> permutations;
+    
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> l = new ArrayList<>();
-        helper(nums,0,l);
-        return l;
+        permutations = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        findPermutations(nums,list);
+        return permutations;
     }
-    public void helper(int[] arr,int ind,List<List<Integer>> l){
-        if(ind==arr.length-1){
-            ArrayList<Integer> array_list = new ArrayList<>();
-            for (int i = 0; i < arr.length; i++)
-                array_list.add(new Integer(arr[i]));
-            l.add(array_list);
-        }
-        for(int i=ind;i<arr.length;i++){
-            arr=swap(arr,i,ind);
-            helper(arr,ind+1,l);
-            arr=swap(arr,i,ind);
+    
+    public void findPermutations(int[] nums, List<Integer> list){
+        if(list.size()==nums.length)
+            permutations.add(new ArrayList<>(list));
+        for(int i=0;i<nums.length;i++){
+            if(list.contains(nums[i]))
+                continue;
+            list.add(nums[i]);
+            findPermutations(nums,list);
+            list.remove(list.size()-1);
         }
     }
-    public int[] swap(int[] arr,int i,int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-        return arr;
-    } 
+    
 }
