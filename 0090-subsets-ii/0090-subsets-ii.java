224bd2748d4590c458b35a41e195c651690d3e62
Such-eth <122589086+Such-eth@users.noreply.github.com>
2023-12-26 13:29:17 +0530

    Time: 1 ms (99.84%), Space: 43.5 MB (83.51%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,26 +1,21 @@
 class Solution {
-    Set<List<Integer>> subsets;
+    List<List<Integer>> subsets;
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        subsets = new HashSet<>();
-        List<Integer> subset = new ArrayList<>();
-        findSubsets(nums,subset,0);
-        List<List<Integer>> ans = new ArrayList<>();
-        for(List<Integer> list:subsets)
-            ans.add(list);
-        return ans;
+        subsets = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<>();
+        findSubsets(nums,0,list,-11);
+        return subsets;
     }
-    public void findSubsets(int[] nums,List<Integer> subset, int index){
-        if(index>=nums.length){
-            subsets.add(new ArrayList<>(subset));
+    public void findSubsets(int[] arr,int index,List<Integer> list,int prev){
+        if(index==arr.length){
+            subsets.add(new ArrayList<>(list));
             return;
         }
-        int current = nums[index++];
-        subset.add(current);
-        // while(index<nums.length && nums[index]==current)
-        //     index++;
-        findSubsets(nums,subset,index);
-        subset.remove(subset.size()-1);
-        findSubsets(nums,subset,index);
+        list.add(arr[index]);
+        findSubsets(arr,index+1,list,arr[index]);
+        list.remove(list.size()-1);
+        if(prev!=arr[index])
+            findSubsets(arr,index+1,list,prev);
     }
 }
