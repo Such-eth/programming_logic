2eee3710744bd8211d4ac070471a351fee795ffa
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-01-16 19:36:48 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (33.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,45 @@
 class Solution {
-    int[] dp;
+    // int[] dp;
     public int rob(int[] nums) {
-        dp = new int[nums.length];
-        for(int i=0;i<dp.length;i++){
-            dp[i]=-1;
+        // dp = new int[nums.length];
+        return find(nums);
+    }
+    // public int find(int[] arr,int ind,int sum){
+    //     if(ind>=arr.length){
+    //         return sum;
+    //     }
+    //     int take = find(arr,ind+2,sum+arr[ind]);
+    //     int notTake = find(arr,ind+1,sum);
+    //     return Math.max(take,notTake);
+    // }
+    // public int find(int[] arr,int ind){
+    //     if(ind>=arr.length)
+    //         return 0;
+    //     if(dp[ind]>0)
+    //         return dp[ind];
+    //     int take = find(arr,ind+2)+arr[ind];
+    //     int notTake = find(arr,ind+1);
+    //     return dp[ind]=Math.max(take,notTake);
+    // }
+    public int find(int[] arr){
+        int[] dp = new int[arr.length+2];
+        for(int ind=arr.length-1;ind>=0;ind--){
+            int take = dp[ind+2]+arr[ind];
+            int notTake = dp[ind+1];
+            dp[ind]=Math.max(take,notTake);
         }
-        return helper(nums,0);
+        return dp[0];
     }
-    public int helper(int[] arr,int n){
-        if(n==arr.length-1)
-            return arr[arr.length-1];
-        if(n>=arr.length-1)
-            return 0;
-        if(dp[n]>=0)
-            return dp[n];
-        dp[n] = Math.max(helper(arr,n+1),arr[n]+helper(arr,n+2));
-        return dp[n];
-    }
+    // public int find(int[] arr){
+    //     int n1=0,n2=0,curr=0;
+    //     for(int ind=arr.length-1;ind>=0;ind--){
+    //         int take = n2+arr[ind];
+    //         int notTake = n1;
+    //         curr=Math.max(take,notTake);
+    //         // curr = dp[ind];
+    //         n1=curr;
+    //         n2=n1;
+    //     }
+    //     return curr;
+    // }
 }
