0eeac44d466e707fd96e62182d55279bd61903d5
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-01-17 21:37:04 +0530

    Time: 1 ms (96.93%), Space: 45.5 MB (29.01%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,28 +1,21 @@
 class Solution {
-    int[][] dp;
     public int minPathSum(int[][] grid) {
-        dp=new int[grid.length][grid[0].length];
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                dp[i][j]=-1;
-            }
-        }
-        return mini(grid,0,0);
+        int[][] dp = new int[grid.length][grid[0].length];
+        return findPathSum(grid,0,0,dp);
     }
-    public int mini(int[][] arr,int i,int j){
-        if(i==arr.length-1 && j==arr[0].length-1)
-            return arr[i][j];
-        if(i>arr.length-1 || j>arr[0].length-1)
-            return Integer.MAX_VALUE;
-        if(dp[i][j]>-1)
-            return dp[i][j];
-        dp[i][j] = arr[i][j]+Math.min(mini(arr,i+1,j),mini(arr,i,j+1));
-        return dp[i][j];
+    public int findPathSum(int[][] arr,int row,int col,int[][] dp){
+        // if(row>=arr.length || col>=arr[0].length){
+        //     return Integer.MAX_VALUE;
+        // }
+        if(dp[row][col]!=0)
+            return dp[row][col];
+        if(row==arr.length-1 && col==arr[0].length-1)
+            return dp[row][col]=arr[row][col];
+        int down=Integer.MAX_VALUE,right=Integer.MAX_VALUE;
+        if(row<arr.length-1)
+             down = findPathSum(arr,row+1,col,dp)+arr[row][col];
+        if(col<arr[0].length-1)
+            right = findPathSum(arr,row,col+1,dp)+arr[row][col];
+        return dp[row][col]=Math.min(down,right);
     }
 }
-// if(m<0 || n<0)
-//             return Integer.MAX_VALUE;
-//         if(m==0 && n==0)
-//             return grid[0][0];
-        
-//         return grid[m][n] + Math.min(find(grid, m-1, n),find(grid, m, n-1)); 
