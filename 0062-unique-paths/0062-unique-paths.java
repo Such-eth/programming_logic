060ef19ecf22eac7a6cd6ba4be639b751072a197
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-01-17 21:02:26 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (36.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,48 +1,17 @@
 class Solution {
-    static int count=0;
-    static int[][] dp;
-    public static int uniquePaths(int m, int n) {
-        count=0;
-        boolean[][] arr = new boolean[m][n];
-        dp=new int[m][n];
-        // helper(arr,0,0);
-        return helper1(arr,0,0);
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        return findPaths(0,0,m,n,dp);
     }
-
-    private static void helper(boolean[][] arr, int i, int j) {
-        if(i>=arr.length || j>=arr[0].length){return;}
-        if(i== arr.length-1 || j==arr[0].length-1){
-            count++;
-            return;
-        }
-        //down
-        if(i< arr.length-1){
-            helper(arr,i+1,j);
-        }
-        //right
-        if(j<arr[0].length-1){
-            helper(arr,i,j+1);
-        }
-    }
-    
-    private static int helper1(boolean[][] arr, int i, int j){
-        if(i>=arr.length || j>=arr[0].length){return 0;}
-        if(i== arr.length-1 || j==arr[0].length-1){
+    public int findPaths(int row,int col,int m, int n,int[][] dp){
+        if(row>=m || col>=n)
+            return 0;
+        if(dp[row][col]!=0)
+            return dp[row][col];
+        if(row==m-1 && col==n-1)
             return 1;
-        }
-        if(dp[i][j]>0){
-            return dp[i][j];
-        }
-        int c=0;
-        //down
-        if(i< arr.length-1){
-            c+=helper1(arr,i+1,j);
-        }
-        //right
-        if(j<arr[0].length-1){
-            c+=helper1(arr,i,j+1);
-        }
-        dp[i][j]=c;
-        return c;
+        int down = findPaths(row+1,col,m,n,dp);
+        int right = findPaths(row,col+1,m,n,dp);
+        return dp[row][col]=down+right;
     }
 }
