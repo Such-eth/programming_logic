c7005adf2032f4ef9b5a339b48fb6a2aa4e7dfd5
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-01-17 21:19:41 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (36.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,7 +1,20 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        return findPaths(0,0,m,n,dp);
+        for(int i=0;i<m;i++){
+            dp[i][n-1]=1;
+        }
+        for(int i=0;i<n;i++){
+            dp[m-1][i]=1;
+        }
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                int down = dp[i+1][j];
+                int right = dp[i][j+1];
+                dp[i][j]=down+right;
+            }
+        }
+        return dp[0][0];
     }
     public int findPaths(int row,int col,int m, int n,int[][] dp){
         if(row>=m || col>=n)
