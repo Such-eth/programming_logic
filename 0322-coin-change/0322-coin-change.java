8a6b7c8a25a67eaedd1b013ea4d6c87c1468ef3b
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-01-20 13:08:30 +0530

    Time: 19 ms (46.45%), Space: 44.4 MB (40.13%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,8 +1,9 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount+1];
-        Arrays.fill(dp, -1);
-        int ans = find(coins, amount,dp);
+        // Arrays.fill(dp, -1);
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        int ans = find1(coins, amount,dp);
         return (ans == Integer.MAX_VALUE) ?  -1 : ans;
     }
     public int find(int[] arr,int amt,int[] dp)
@@ -20,5 +21,17 @@ class Solution {
                 take = Math.min(take,1+ans);
         }
         return dp[amt]=take;
+    }
+    public int find1(int[] arr,int amount,int[] dp){
+        dp[0] = 0;
+        for(int amt=1;amt<dp.length;amt++){
+            int take=Integer.MAX_VALUE;
+            for(int i=0;i<arr.length;i++){
+                if(amt - arr[i] >= 0 && dp[amt - arr[i]] != Integer.MAX_VALUE) {
+                    dp[amt] = Math.min(dp[amt], 1 + dp[amt - arr[i]]);
+                }
+            }
+        }
+        return dp[amount];
     }
 }
