e607438b17403ae06da8a88a3dded9767239b25c
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-01-22 12:20:20 +0530

    Time: 7 ms (53.82%), Space: 48 MB (16.32%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -3,22 +3,36 @@ class Solution {
     public int change(int amount, int[] coins) {
         // Arrays.sort(coins);
         dp = new int[coins.length][amount+1];
-        for(int[] i:dp)
-            Arrays.fill(i,-1);
-        return find(coins,coins.length-1,amount);
-    }
-    public int find(int[] arr,int ind,int amt){
-        if(ind==0){
-            if(amt%arr[ind]==0)
-                return dp[ind][amt]=1;
-            return dp[ind][amt]=0;
+        // for(int[] i:dp)
+        //     Arrays.fill(i,-1);
+        for(int i=0;i<amount+1;i++){
+            if(i%coins[0]==0)
+                dp[0][i]=1;
+            else dp[0][i]=0;
         }
-        if(dp[ind][amt]!=-1)
-            return dp[ind][amt];
-        int notTake = find(arr,ind-1,amt);
-        int take=0;
-        if(arr[ind]<=amt)
-            take = find(arr,ind,amt-arr[ind]);
-        return dp[ind][amt]=take+notTake;
+        for(int ind=1;ind<coins.length;ind++){
+            for(int amt=0;amt<=amount;amt++){
+                int notTake = dp[ind-1][amt];
+                int take=0;
+                if(coins[ind]<=amt)
+                    take = dp[ind][amt-coins[ind]];
+                dp[ind][amt]=take+notTake;
+            }
+        }
+        return dp[coins.length-1][amount];
     }
+    // public int find(int[] arr,int ind,int amt){
+    //     if(ind==0){
+    //         if(amt%arr[ind]==0)
+    //             return dp[ind][amt]=1;
+    //         return dp[ind][amt]=0;
+    //     }
+    //     if(dp[ind][amt]!=-1)
+    //         return dp[ind][amt];
+    //     int notTake = find(arr,ind-1,amt);
+    //     int take=0;
+    //     if(arr[ind]<=amt)
+    //         take = find(arr,ind,amt-arr[ind]);
+    //     return dp[ind][amt]=take+notTake;
+    // }
 }
