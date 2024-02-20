851af4fa2e4a0838aae2f5719539c26dbb925920
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-02-20 19:40:48 +0530

    Time: 1 ms (99.69%), Space: 57.6 MB (5.39%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max=Integer.MIN_VALUE;
+        int globalSum=nums[0];
         int sum=0;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            max=Math.max(max,sum);
-            if(sum<0){
-                sum=0;
-            }
+            globalSum = Math.max(sum,globalSum);
+            if(sum<0)
+                sum=0;            
         }
-        return max;
+        return globalSum;
     }
 }
