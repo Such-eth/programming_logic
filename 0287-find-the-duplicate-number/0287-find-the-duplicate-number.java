330df7d61edfae103324d85be1cd666f89cfd94a
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-02-22 14:20:54 +0530

    Time: 4 ms (87.67%), Space: 57.6 MB (80.33%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,15 +1,12 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int s=nums[0],f=nums[0];
-        do{
-            s=nums[s];
-            f=nums[nums[f]];
-        }while(s!=f);
-        s=nums[0];
-        while(s!=f){
-            s=nums[s];
-            f=nums[f];
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int ind = n-Math.abs(nums[i])-1;
+            if(nums[ind]<0)
+                return Math.abs(nums[i]);
+            nums[ind]=-nums[ind];
         }
-        return f;
+        return -1;
     }
 }
