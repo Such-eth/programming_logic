af4b5546c18cfc0f7041dad379a9bd2628cb12e0
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-03-04 15:39:34 +0530

    Time: 3 ms (5.53%), Space: 42.1 MB (42.45%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,49 +1,32 @@
 class Solution {
-    public int search(int[] arr, int target) {
-        if(arr.length==1){
-            if(arr[0]==target){
-                return 0;
-            }else{
-                return -1;
-            }
-        }
-        int p=pivot(arr);
-        int a=ordr_eg_bs(arr,target,0,p);
-        if(a!=-1){
-            return a;
-        }else{
-            return (ordr_eg_bs(arr,target,p+1, arr.length-1));
-        }
-    }
-    private static int ordr_eg_bs(int[] arr, int target, int start, int end) {
-        while(start<=end){
-            int mid=start-(start-end)/2;
-            if(arr[mid]>target){
-                end=mid-1;
-            }
-            else if(arr[mid]<target){
-                start=mid+1;
-            }else{
-                return mid;
-            }
-        }
-        return -1;
-    }
-
-    private static int pivot(int[] arr) {
+    public int search(int[] nums, int target) {
         int start=0;
-        int end=arr.length-1;
+        int end = nums.length-1;
+        int pivot=-1;
         while(start<=end){
-            int mid=start-(start-end)/2;
-            if(mid<end && arr[mid]>arr[mid+1]){
-                return mid;
-            }else if(mid>start && arr[mid]<arr[mid-1]){
-                return mid-1;
-            }else if(arr[start]>arr[mid]){
-                end=mid-1;
-            }else{
-                start=mid+1;
+            int mid = start-(start-end)/2;
+            if(mid!=nums.length-1 && nums[mid]>nums[mid+1]){
+                pivot=mid;
+                break;
             }
+            if(nums[mid]>=nums[0])
+                start=mid+1;
+            else end=mid-1;
+        }
+        System.out.println(pivot);
+        int ans=binarySearch(nums,0,pivot,target);
+        if(ans!=-1)
+            return ans;
+        return binarySearch(nums,pivot+1,nums.length-1,target);
+    }
+    public int binarySearch(int[] nums,int start,int end,int target){
+        while(start<=end){
+            int mid = start-(start-end)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[mid]<target)
+                start=mid+1;
+            else end=mid-1;
         }
         return -1;
     }
