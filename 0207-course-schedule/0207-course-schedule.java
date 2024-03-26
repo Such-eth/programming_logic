b3d71c9581911f8c9dc8e56b43fd15409fa97b71
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-03-26 17:30:38 +0530

    Time: 3 ms (94.81%), Space: 44.4 MB (77.32%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,35 +1,34 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] pre) {
-        List<List<Integer>> graph = new ArrayList<>();
+        List<List<Integer>> g = new ArrayList<>();
         for(int i=0;i<numCourses;i++){
-            graph.add(new ArrayList<>());
+            g.add(new ArrayList<>());
         }
         for(int i=0;i<pre.length;i++){
-            graph.get(pre[i][1]).add(pre[i][0]);
+            g.get(pre[i][1]).add(pre[i][0]);
         }
-        int[] indegree = new int[numCourses];
+        int[] vis = new int[numCourses];
+        int[] path = new int[numCourses];
         for(int i=0;i<numCourses;i++){
-            for(int it : graph.get(i)){
-                indegree[it]++;
+            if(vis[i]==0)
+                if(check(g,vis,path,i)==true)
+                    return false;
+        }
+        
+        return true;
+    }
+    public boolean check(List<List<Integer>> g, int[] vis,int[] path, int ind){
+        vis[ind] = 1;
+        path[ind]= 1;
+        for(int i:g.get(ind)){
+            if(vis[i]==0){
+                if(check(g,vis,path,i)) return true;
+            }else if(path[i]==1){
+                return true;
             }
         }
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0)
-                q.add(i);
-        }
-        int count=0;
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            for(int it : graph.get(curr)){
-                indegree[it]--;
-                if(indegree[it]==0)
-                    q.add(it);
-            }
-            count++;
-        }
-        if(count==numCourses)
-            return true;
+        path[ind]=0;
         return false;
     }
+    
 }
