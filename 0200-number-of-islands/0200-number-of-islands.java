27cee3dc54cc42296a62685ab8a78bf766a7549a
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-03-26 18:25:53 +0530

    Time: 4 ms (48.37%), Space: 51.5 MB (20.67%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,43 +1,31 @@
 class Solution {
-    class Node{
-        int f,s;
-        public Node(int a,int s){
-            this.f=a;
-            this.s=s;
-        }
-    }
-
-    public int numIslands(char[][] grid){
+    public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        Queue<Node> q = new LinkedList<>();
-        int ans=0;
         int[][] vis = new int[m][n];
-        for (int i=0;i<m;i++){
+        int ans = 0;
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='1' && vis[i][j]!=1){
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    dfs(grid,vis,i,j);
                     ans++;
-                    vis[i][j]=1;
-                    q.offer(new Node(i,j));
-                    while(!q.isEmpty()){
-                        Node node = q.poll();
-                        int row = node.f;
-                        int col = node.s;
-                        int[] drow = {-1,0,1,0};
-                        int[] dcol = {0,1,0,-1};
-                        for(int k=0;k<4;k++){
-                            int r = row+drow[k];
-                            int c = col+dcol[k];
-                            if(r>=0 && r<m && c>=0 && c<n && grid[r][c]=='1' && vis[r][c]==0){
-                                vis[r][c]=1;
-                                q.add(new Node(r,c));
-                                
-                            }
-                        }
-                    }
                 }
             }
         }
         return ans;
+    }
+    public void dfs(char[][] g, int[][] vis,int i,int j){
+        vis[i][j]=1;
+        int row = i;
+        int col = j;
+        int[] drow = {-1,0,1,0};
+        int[] dcol = {0,1,0,-1};
+        for(int k=0;k<4;k++){
+            int r = row+drow[k];
+            int c = col+dcol[k];
+            if(r>=0 && r<g.length && c>=0 && c<g[0].length && g[r][c]=='1' && vis[r][c]==0){
+                dfs(g,vis,r,c);
+            }
+        }
     }
 }
