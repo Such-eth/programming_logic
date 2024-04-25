35722b2029f43670217afc317e47c9ff52edac4e
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-04-25 14:48:39 +0530

    Time: 60 ms (12.69%), Space: 49.1 MB (14.58%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    function toBe(val2){
+        if(val===val2)
+            return true;
+        else throw new Error("Not Equal");
+    }
+    function notToBe(val2){
+        if(val!==val2)
+            return true;
+        else throw new Error("Equal");
+    }
+    return{
+        toBe:toBe,
+        notToBe:notToBe
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
