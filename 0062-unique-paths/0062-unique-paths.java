3c811df52a10920a1b695d0ad98384dc1cf6e3df
Such-eth <122589086+Such-eth@users.noreply.github.com>
2024-06-18 16:38:11 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (48.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,30 +1,25 @@
 class Solution {
+    int[][] dp;
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            dp[i][n-1]=1;
-        }
-        for(int i=0;i<n;i++){
-            dp[m-1][i]=1;
-        }
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                int down = dp[i+1][j];
-                int right = dp[i][j+1];
-                dp[i][j]=down+right;
+        dp = new int[m][n];
+        for(int i=0;i<n;i++)
+            dp[0][i]=1;
+        for(int i=0;i<m;i++)
+            dp[i][0]=1;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                int left = dp[i][j-1];
+                int topLeft = dp[i-1][j];
+                dp[i][j]=left+topLeft;
             }
         }
-        return dp[0][0];
+        return dp[m-1][n-1];
     }
-    public int findPaths(int row,int col,int m, int n,int[][] dp){
-        if(row>=m || col>=n)
-            return 0;
-        if(dp[row][col]!=0)
-            return dp[row][col];
-        if(row==m-1 && col==n-1)
-            return 1;
-        int down = findPaths(row+1,col,m,n,dp);
-        int right = findPaths(row,col+1,m,n,dp);
-        return dp[row][col]=down+right;
-    }
+    // public int find(int i,int j){
+    //     if(i==0||j==0) return dp[i][j]=1;
+    //     if(dp[i][j]!=0) return dp[i][j];
+    //     int left = find(i,j-1);
+    //     int topLeft = find(i-1,j);
+    //     return dp[i][j]=left+topLeft;
+    // }
 }
